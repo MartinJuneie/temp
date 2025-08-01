45f5482cb3179dd97cbe1bcfcb04c0d0cc3423bb
Martin Joseph Juneie <37549926+MartinJuneie@users.noreply.github.com>
2025-08-01 17:18:21 +0530

    Update tempscript.py

--- .github/workflows/tempscript.py
+++ .github/workflows/tempscript.py
@@ -1,1 +1,17 @@
-print ("Hello")
+import json
+
+# Specify the path to your JSON file
+filename = 'temp.json'
+
+try:
+    # Open and load the JSON file
+    with open(filename, 'r') as file:
+        data = json.load(file)
+    
+    # Pretty-print the JSON data
+    print(json.dumps(data, indent=4))
+
+except FileNotFoundError:
+    print(f"File '{filename}' not found.")
+except json.JSONDecodeError as e:
+    print(f"Error decoding JSON: {e}")
